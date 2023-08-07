a74142736c463463ce8a883491acd6ab755b6ec0
Harsh Baraiya <baraiyaharsh319@gmail.com>
2023-08-07 16:40:28 +0530

    code upadeted

--- src/actions/cartAction.ts
+++ src/actions/cartAction.ts
@@ -1,13 +1,45 @@
 import axiosInstance from "../axiosInstance.ts";
 
-export const loardCartAction = () => async (dispatch) => {
+export const loadCartAction = () => async (dispatch) => {
   try {
-    dispatch({ type: "LOAD_Cart_REQUEST" });
+    dispatch({ type: "LOAD_CART_REQUEST" });
     const url = "660/cart";
     const res = await axiosInstance.get(url);
-    dispatch({ type: "LOAD_Cart_Sucess", payload: res });
+    dispatch({ type: "LOAD_CART_SUCCESS", payload: res });
   } catch (error) {
-    console.log(error);
-    dispatch({ type: "LOAD_Cart_Fail", payload: error });
+    dispatch({ type: "LOAD_CART_FAIL", payload: error });
+  }
+};
+
+export const AddItemCartAction = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: "ADD_ITEM_CART_REQUEST" });
+    const url = "660/cart";
+    const res = await axiosInstance.post(url, data);
+    dispatch({ type: "ADD_ITEM_CART_SUCCESS", payload: res });
+  } catch (error) {
+    dispatch({ type: "ADD_ITEM_CART_FAIL", payload: error });
+  }
+};
+
+export const updateCartAction = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: "UPDATE_ITEM_CART_REQUEST" });
+    const url = `660/cart/${data.id}`;
+    const res = await axiosInstance.put(url, data);
+    dispatch({ type: "UPDATE_ITEM_CART_SUCCESS", payload: res });
+  } catch (error) {
+    dispatch({ type: "UPDATE_ITEM_CART_FAIL", payload: error });
+  }
+};
+
+export const deleteCartAction = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: "DELETE_ITEM_CART_REQUEST" });
+    const url = `660/cart/${data.id}`;
+    await axiosInstance.delete(url);
+    dispatch({ type: "DELETE_ITEM_CART_SUCCESS", payload: data });
+  } catch (error) {
+    dispatch({ type: "DELETE_ITEM_CART_FAIL", payload: error });
   }
 };
--- src/reducer/index.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import cart from "./cartReducer.ts";
-import product from "./productReducer.ts";
-export default combineReducers({
-    cart,
-    product
-})
